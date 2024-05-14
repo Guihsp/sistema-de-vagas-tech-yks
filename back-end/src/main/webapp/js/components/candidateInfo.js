c975aa7eb4659a9302605bb08e5acc9dfcbdd197
Guilherme Henrique S. Pereira <guilhermehsp26@gmail.com>
2024-05-14 02:01:22 -0300

    feat: Add candidate profile page and styles

---
+++ back-end/src/main/webapp/js/components/candidateInfo.js
@@ -0,0 +1,27 @@
+function createInfoCompanyComponent() {
+    const userProfile = `
+        <div class="container">
+            <div class="user-infos">
+                <div class="user-img">
+                    <img src="./assets/profile.svg" alt="Foto de perfil">
+                </div>
+                
+                <div class="user-data">
+                    <p id="user-name">Guilherme henrique</p>
+                    <img src="./assets/Location.svg" alt="Localização">
+                    <span id="user-location">São Paulo - SP</span>
+                </div>
+            </div>
+
+            <div class="box-btn">
+                <a href="" class="btn">Editar Perfil</a>
+                <a href="" class="btn">Minhas Vagas</a>
+            </div>
+        </div>
+    `;
+
+    return userProfile;
+}
+
+const userProfileComponent = createInfoCompanyComponent();
+document.querySelector('.section-user-infos').innerHTML = userProfileComponent;
