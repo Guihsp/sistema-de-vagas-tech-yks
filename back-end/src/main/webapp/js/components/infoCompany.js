9017a363f2e344eae05ccb2fe10ce7a37468b6a7
vitor Cobeio <vitorcobeio7@gmail.com>
2024-05-17 12:35:59 -0300

    Criação de compenetes da pagina companyProfile

--- back-end/src/main/webapp/js/components/infoCompany.js
+++ back-end/src/main/webapp/js/components/infoCompany.js
@@ -1,58 +1,52 @@
-function createCandidateDescriptionContact(candidate) {
+function createCompanyDescription(company) {
     const container = document.querySelector('.container')
 
-    const descriptionContact = document.createElement('div');
-    descriptionContact.className = 'candidate-description-contact';
+    const descriptionCompany = document.createElement('div');
+    descriptionCompany.className = 'description-company';
 
-    descriptionContact.innerHTML = `
-        <h1>Descrição</h1>
+    descriptionCompany.innerHTML = `
+        <h1>Descrição da Empresa</h1>
         <p id="description">
-            ${candidate.information}
-        </p>
-
-        <h2>Contato</h2>
-        <p class="contact">
-            <strong>Email:</strong>
-            <span id="email">${candidate.email}</span>
+            ${company.information}
         </p>
     `;
 
-    container.appendChild(descriptionContact);
+    container.appendChild(descriptionCompany);
     return container;
 }
 
-const getCandidateById = async () => {
-    let candidateId;
+const getCompanyeById = async () => {
+    let companyId;
 
-    if (localStorage.getItem('userId')) {
-        candidateId = localStorage.getItem('userId');
-    } else if (localStorage.getItem('user')) {
-        const user = JSON.parse(localStorage.getItem('user'));
-        candidateId = user.id;
+    if (localStorage.getItem('companyId')) {
+        companyIdId = localStorage.getItem('companyId');
+    } else if (localStorage.getItem('company')) {
+        const company = JSON.parse(localStorage.getItem('company'));
+        companyId = company.id;
     } else {
-        console.error("Usuário não encontrado.");
+        console.error("Empresa não encontrado.");
         return;
     }
 
     return new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest();
-        const url = `http://localhost:8080/api/getUserById/${candidateId}`;
+        const url = `http://localhost:8080/api/getCompanyById/${companyId}`;
 
         xhr.open("GET", url, true);
         xhr.setRequestHeader("Content-Type", "application/json");
 
         xhr.onload = function () {
             if (xhr.status === 200) {
-                const candidate = JSON.parse(xhr.responseText);
-                return resolve(candidate);
+                const company = JSON.parse(xhr.responseText);
+                return resolve(company);
             } else {
-                console.error("Erro ao buscar candidato:", xhr.responseText);
+                console.error("Erro ao buscar empresa:", xhr.responseText);
                 reject(xhr.responseText);
             }
         };
 
         xhr.onerror = function () {
-            reject("Erro de rede ao buscar candidato.");
+            reject("Erro de rede ao buscar empresa.");
         };
 
         xhr.send();
@@ -60,15 +54,15 @@ const getCandidateById = async () => {
 }
 
 const application = async () => {
-    const candidate = await getCandidateById();
-    createCandidateDescriptionContact(candidate);
+    const company = await getCompanyById();
+    createCompanyDescription(company);
 
     if (!localStorage.getItem('company')) {
         const header = document.createElement('script');
-        header.src = './js/components/headerCandidate.js';
+        header.src = './js/components/headerCompany.js';
         document.body.appendChild(header);
         const infoUser = document.createElement('script');
-        infoUser.src = './js/components/candidateInfo.js';
+        infoUser.src = './js/components/companyInfo.js';
         document.body.appendChild(infoUser);
     } else {
         const header = document.createElement('script');
@@ -78,7 +72,7 @@ const application = async () => {
         infoUser.src = './js/components/candidateInfoToCompany.js';
         document.body.appendChild(infoUser);
         localStorage.removeItem('user');
-        localStorage.setItem('user', JSON.stringify(candidate));
+        localStorage.setItem('user', JSON.stringify(company));
     }
 }
 
